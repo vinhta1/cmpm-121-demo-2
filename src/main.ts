c33441883b6cd65ef7996b232337cef1c8164d6c
Vinh Ta <tavinh132@gmail.com>
2024-10-28 18:33:47 -0700

    Step 1: Initial non-interactive UI layout complete

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,21 @@ import "./style.css";
 
 const APP_NAME = "Sketch-a-Stick";
 const app = document.querySelector<HTMLDivElement>("#app")!;
-
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+const appTitle = document.createElement("h1");
+appTitle.innerHTML = APP_NAME;
+
+//Constants
+
+
+//The Canvas
+    function draw(){
+    const canvas = document.getElementById("canvas");
+    if (canvas.getContext){
+        const context = canvas.getContext("2d");
+    }
+}
+
+//Main
+draw();
+app.append(appTitle);
